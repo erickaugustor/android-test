ef7b9bbf45d5fc37bb43d542f4c5a2945f3bf184
erickaugusto <erick.augustoramos@gmail.com>
2017-12-02 12:09:53 -0200

    version 0.0.5 - navigate between activities

--- app/src/main/java/com/example/erickaugusto/fofis/MainActivity.java
+++ app/src/main/java/com/example/erickaugusto/fofis/MainActivity.java
@@ -1,16 +1,39 @@
 package com.example.erickaugusto.fofis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button ButtonAnotherActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toast.makeText(MainActivity.this, "OnCreate Called", Toast.LENGTH_SHORT).show();
+
+        ButtonAnotherActivity = findViewById(R.id.buttonAnotherActivity);
+        ButtonAnotherActivity.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View w){
+               //Intent é uma ação, o que o aplicativo deverá fazer
+
+                /* Você pode fazer isso:
+                Intent intent = new Intent(MainActivity.this, AnotherActivity.class);
+                startActivity(intent);
+
+                Ou isso: */
+
+                startActivity(new Intent(MainActivity.this, AnotherActivity.class));
+
+            }
+        });
     }
 
     @Override
